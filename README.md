84becc050665836c1e5b0e5df5b945074cf7930d
dirkarnez <smalldirkalex@gmail.com>
2022-07-18 20:47:20 +0800

    - revamp

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 glfw-boilerplate
 ================
+`transparency_glfw.cpp` not yet working, maybe need extra configuration. From [Transparent framebuffer and borderless window using GLFW](https://ecode.dev/transparent-framebuffer-borderless-window-using-glfw/)
--- local-build.cmd
+++ local-build.cmd
@@ -3,12 +3,19 @@ REM run as Administrator
 cd /d %~dp0
 set DOWNLOAD_DIR=%USERPROFILE%\Downloads
 set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
-SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\mingw64-win32;%DOWNLOAD_DIR%\mingw64-win32\bin;%DOWNLOAD_DIR%\cmake-3.22.0-rc1-windows-x86_64\bin;
-cmake.exe -G"MinGW Makefiles" ^
--B./build
-cd build
+@REM SET PATH=^
+@REM %DOWNLOAD_DIR%\PortableGit\bin;^
+@REM %DOWNLOAD_DIR%\x86_64-8.1.0-release-win32-seh-rt_v6-rev0;^
+@REM %DOWNLOAD_DIR%\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\bin;^
+@REM %DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;^
 
-:rebuild_and_startapp
-mingw32-make.exe
+set PATH=^
+D:\Softwares\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\mingw64;^
+D:\Softwares\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\mingw64\bin;^
+D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
+
+cmake.exe -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug -DGLAD2_ROOT="D:/Code Projects/cpp-libraries/glad2-web-generated" -DGLFW_ROOT="D:/Code Projects/cpp-libraries/glfw-3.3.7.bin.WIN64" -B./build &&^
+cd build &&^
+cmake --build . &&^
+echo "Successful build"
 pause
-REM GOTO rebuild_and_startapp
